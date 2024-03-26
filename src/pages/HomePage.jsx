01971d6397492c4de49e4a915dbf1ca567872c2f
Meena Chand <meena.hgmd@gmail.com>
2024-03-26 08:29:28 -0700

    categories component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React, { Component, Fragment } from 'react';
 import FeaturedProduct from '../components/home/FeaturedProduct';
+import Categories from '../components/home/Categories';
 
 export class HomePage extends Component {
   render() {
     return (
         <Fragment>
             <FeaturedProduct/>
+            <Categories/>
         </Fragment>
     )
   }
