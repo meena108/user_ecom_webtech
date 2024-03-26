6f6c6c741d3bb0b910530195df83175a65ca9d02
Meena Chand <meena.hgmd@gmail.com>
2024-03-26 10:33:30 -0700

    accordian side menu

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,11 +3,13 @@ import Categories from "../components/home/Categories";
 import FeaturedProducts from "../components/home/FeaturedProduct";
 import Collection from "../components/home/Collection";
 import NewArrival from "../components/home/NewArrival";
+import HomeTop from "../components/home/HomeTop";
 
 export class HomePage extends Component {
   render() {
     return (
       <Fragment>
+        <HomeTop />
         <FeaturedProducts />
         <NewArrival />
         <Collection />
