9c8b9aab769fef5634995a24deea08308f5066c0
Meena Chand <meena.hgmd@gmail.com>
2024-03-26 09:12:44 -0700

    New Arrival produsts.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,14 @@ import React, { Component, Fragment } from "react";
 import Categories from "../components/home/Categories";
 import FeaturedProducts from "../components/home/FeaturedProduct";
 import Collection from "../components/home/Collection";
+import NewArrival from "../components/home/NewArrival";
 
 export class HomePage extends Component {
   render() {
     return (
       <Fragment>
         <FeaturedProducts />
+        <NewArrival />
         <Collection />
         <Categories />
       </Fragment>
