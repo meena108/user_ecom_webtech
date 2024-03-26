4500b0e945e657d6ac930b8bef1c960f7d891054
Meena Chand <meena.hgmd@gmail.com>
2024-03-26 10:56:38 -0700

    NAVBAR ADDED

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,16 +4,18 @@ import FeaturedProducts from "../components/home/FeaturedProduct";
 import Collection from "../components/home/Collection";
 import NewArrival from "../components/home/NewArrival";
 import HomeTop from "../components/home/HomeTop";
+import NavMenuDesktop from "../components/common/NavMenuDesktop";
 
 export class HomePage extends Component {
   render() {
     return (
       <Fragment>
+        <NavMenuDesktop />
         <HomeTop />
         <FeaturedProducts />
         <NewArrival />
-        <Collection />
         <Categories />
+        <Collection />
       </Fragment>
     );
   }
