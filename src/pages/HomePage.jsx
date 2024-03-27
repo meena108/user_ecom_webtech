93a94b3c2ab54633b3223ac49912482e86869d85
Meena Chand <meena.hgmd@gmail.com>
2024-03-27 08:03:03 -0700

    mobile view and desktop view

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,13 +5,23 @@ import Collection from "../components/home/Collection";
 import NewArrival from "../components/home/NewArrival";
 import HomeTop from "../components/home/HomeTop";
 import NavMenuDesktop from "../components/common/NavMenuDesktop";
+import NavMenuMobile from "../components/common/NavMenuMobile";
+import HomeTopMobile from "../components/home/HomeTopMobile";
 
 export class HomePage extends Component {
   render() {
     return (
       <Fragment>
-        <NavMenuDesktop />
-        <HomeTop />
+        <div className="Desktop">
+          <NavMenuDesktop />
+          <HomeTop />
+        </div>
+
+        <div className="Mobile">
+          <NavMenuMobile />
+          <HomeTopMobile />
+        </div>
+
         <FeaturedProducts />
         <NewArrival />
         <Categories />
