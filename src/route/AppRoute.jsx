f2b4c57f1d9d2518b94115b18f54866f6d983d14
Meena Chand <meena.hgmd@gmail.com>
2024-03-29 09:57:22 -0700

    Userlogin page

--- src/route/AppRoute.jsx
+++ src/route/AppRoute.jsx
@@ -1,17 +1,19 @@
-import React, { Component, Fragment } from 'react';
-import HomePage from '../pages/HomePage';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Component, Fragment } from "react";
+import { Router, Route, Switch } from "react-router";
+import HomePage from "../pages/HomePage";
+import UserLoginPage from "../pages/UserLoginPage";
 
 class AppRoute extends Component {
-    render() {
-        return (
-            <Fragment>
-                <Switch>
-                    <Route exact path="/" component={HomePage} />
-                </Switch>
-            </Fragment>
-        ); // Add closing tag for Fragment component
-    }
+  render() {
+    return (
+      <Fragment>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/login" component={UserLoginPage} />
+        </Switch>
+      </Fragment>
+    );
+  }
 }
 
 export default AppRoute;
