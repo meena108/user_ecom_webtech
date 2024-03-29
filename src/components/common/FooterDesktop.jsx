d03ca095e42b91f5fad034a54ad087d19e51c4b9
Meena Chand <meena.hgmd@gmail.com>
2024-03-29 10:28:55 -0700

    refund, purchase page made

--- src/components/common/FooterDesktop.jsx
+++ src/components/common/FooterDesktop.jsx
@@ -33,17 +33,14 @@ export class FooterDesktop extends Component {
               <Col className="p-2" lg={3} md={3} sm={6} xs={12}>
                 <h5 className="footer-menu-title">THE COMPANY</h5>
                 <Link to="/" className="footer-link">
-                  {" "}
                   About Us
                 </Link>
                 <br></br>
                 <Link to="/" className="footer-link">
-                  {" "}
                   Company Profile
                 </Link>
                 <br></br>
-                <Link to="/" className="footer-link">
-                  {" "}
+                <Link to="/contact" className="footer-link">
                   Contact Us
                 </Link>
                 <br></br>
@@ -51,18 +48,16 @@ export class FooterDesktop extends Component {
 
               <Col className="p-2" lg={3} md={3} sm={6} xs={12}>
                 <h5 className="footer-menu-title">MORE INFO</h5>
-                <Link to="/" className="footer-link">
+                <Link to="/purchase" className="footer-link">
                   How To Purchase
                 </Link>
                 <br></br>
-                <Link to="/" className="footer-link">
-                  {" "}
+                <Link to="/privacy" className="footer-link">
                   Privacy Policy
                 </Link>
                 <br></br>
-                <Link to="/" className="footer-link">
-                  {" "}
-                  Refund Policy{" "}
+                <Link to="/refund" className="footer-link">
+                  Refund Policy
                 </Link>
                 <br></br>
               </Col>
