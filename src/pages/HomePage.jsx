032de3a0628e094f683b943fc65c80492198bdf3
Meena Chand <meena.hgmd@gmail.com>
2024-03-30 12:54:47 -0700

    api configure inuser interface

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,8 +9,18 @@ import NavMenuMobile from "../components/common/NavMenuMobile";
 import HomeTopMobile from "../components/home/HomeTopMobile";
 import FooterDesktop from "../components/common/FooterDesktop";
 import FooterMobile from "../components/common/FooterMobile";
+import AppUrl from "../api/AppUrl";
+import axios from "axios";
 
 export class HomePage extends Component {
+  componentDidMount() {
+    window.scroll(0, 0);
+    this.GetVisitorDetails();
+  }
+
+  GetVisitorDetails = () => {
+    axios.get(AppUrl.VisitorDetails).then().catch();
+  };
   render() {
     return (
       <Fragment>
